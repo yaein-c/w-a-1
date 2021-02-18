7975dbea783d22f79cc259d8415b9dce81c37777
Yan <yaneechin@gmail.com>
2021-02-18 14:16:51 +0000

    Add files via upload

---
+++ rock-paper-scissors/app.js
@@ -0,0 +1,22 @@
+const userScore = 0;
+const computerScore = 0;
+
+const userScore_span = document.getElementById("user-score");
+const computerScore_Span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+rock_div.addEventListener('click', function() {
+    console.log("hey you clicked on rock ")
+})
+
+paper_div.addEventListener('click', function() {
+    console.log("hey you clicked on paper ")
+})
+
+scissors_div.addEventListener('click', function() {
+    console.log("hey you clicked on scissors ")
+})
